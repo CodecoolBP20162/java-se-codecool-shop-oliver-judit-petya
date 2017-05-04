aaa4ce18dc1971617a18fc620c6509388d395af7
okocsis90 <o.kocsis90@gmail.com>
2017-05-04 14:06:47 +0200

    ordering more items at once now available

--- src/main/java/com/codecool/shop/model/Order.java
+++ src/main/java/com/codecool/shop/model/Order.java
@@ -33,11 +33,12 @@ public class Order {
         return orderQuantity;
     }
 
-    public int getId() {
-        return id;
-    }
 
     public void setId(int id) {
         this.id = id;
+    }
+
+    public int getId() {
+        return id;
     }
 }
