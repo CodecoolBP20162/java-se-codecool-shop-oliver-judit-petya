87d615f18e5ff06d347208f189cef65c2723061c
petya <szrk.peter@gmail.com>
2017-05-15 13:10:02 +0200

    foregin key problem in init_db.sql

--- src/main/sql/init_db.sql
+++ src/main/sql/init_db.sql
@@ -1,8 +1,22 @@
-DROP TABLE IF EXISTS ProductCategory;
-
+DROP TABLE IF EXISTS ProductCategory, Product, Supplier;
+CREATE TABLE Product(
+  id INT PRIMARY KEY,
+  name VARCHAR(35),
+  description VARCHAR(255),
+  currency VARCHAR(10),
+  default_price FLOAT,
+  FOREIGN KEY (supplier_id) REFERENCES Supplier (id),
+  FOREIGN KEY (category_id) REFERENCES ProductCategory (id)
+);
 CREATE TABLE ProductCategory (
   id INT PRIMARY KEY,
-  name VARCHAR(15),
+  name VARCHAR(35),
   description VARCHAR(50),
-  department VARCHAR(15)
+  department VARCHAR(255)
 );
+
+CREATE TABLE Supplier (
+  id INT PRIMARY KEY,
+  name VARCHAR(35),
+  description VARCHAR(255)
+)
