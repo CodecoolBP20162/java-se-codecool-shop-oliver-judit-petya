2ad5300451ed3ffdd43be9b32a047947902082e4
petya <szrk.peter@gmail.com>
2017-05-15 13:14:16 +0200

    init_db.sql is ready to drop then build the tables

--- src/main/sql/init_db.sql
+++ src/main/sql/init_db.sql
@@ -1,13 +1,5 @@
 DROP TABLE IF EXISTS ProductCategory, Product, Supplier;
-CREATE TABLE Product(
-  id INT PRIMARY KEY,
-  name VARCHAR(35),
-  description VARCHAR(255),
-  currency VARCHAR(10),
-  default_price FLOAT,
-  FOREIGN KEY (supplier_id) REFERENCES Supplier (id),
-  FOREIGN KEY (category_id) REFERENCES ProductCategory (id)
-);
+
 CREATE TABLE ProductCategory (
   id INT PRIMARY KEY,
   name VARCHAR(35),
@@ -19,4 +11,16 @@ CREATE TABLE Supplier (
   id INT PRIMARY KEY,
   name VARCHAR(35),
   description VARCHAR(255)
-)
+);
+
+CREATE TABLE Product(
+  id INT PRIMARY KEY,
+  supplier_id INT,
+  product_category_id INT,
+  name VARCHAR(35),
+  description VARCHAR(255),
+  currency VARCHAR(10),
+  default_price FLOAT,
+  FOREIGN KEY (supplier_id) REFERENCES Supplier (id),
+  FOREIGN KEY(product_category_id) REFERENCES ProductCategory (id)
+);
