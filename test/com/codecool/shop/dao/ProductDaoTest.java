3114df99e32bd0adc26d5f374158502efd4246e8
petya <szrk.peter@gmail.com>
2017-05-16 18:43:21 +0200

    ProductDaoTest class on 100% codecoverage

--- test/com/codecool/shop/dao/ProductDaoTest.java
+++ test/com/codecool/shop/dao/ProductDaoTest.java
@@ -1,8 +1,6 @@
 package com.codecool.shop.dao;
 
-import com.codecool.shop.dao.memImplementation.ProductCategoryDaoMem;
 import com.codecool.shop.dao.memImplementation.ProductDaoMem;
-import com.codecool.shop.dao.memImplementation.SupplierDaoMem;
 import com.codecool.shop.model.Product;
 import com.codecool.shop.model.ProductCategory;
 import com.codecool.shop.model.Supplier;
@@ -13,86 +11,111 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
 
-import static com.codecool.shop.dao.SupplierDaoTest.supplierDataStore;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ProductDaoTest {
 
-    static ProductDao productDataStore = ProductDaoMem.getInstance();
-    static Supplier amazon;
-    static Supplier lenovo;
-    static ProductCategory tablet;
-    static ProductCategory phone;
-    static ProductCategory notebook;
+    private static ProductDao productDataStore = ProductDaoMem.getInstance();
+    private static Supplier amazon;
+    private static ProductCategory tablet;
+
+
+    private List<Product> prepareProductDataStoreWithOneProduct() {
+        Product fire = new Product("Amazon Fire", 49.9f, "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", tablet, amazon);
+        List<Product> products = new ArrayList<>();
+        products.add(fire);
+        productDataStore.add(fire);
+        return products;
+    }
 
     @BeforeAll
-    public void setupAll(){
+    public static void setupAll() {
         amazon = new Supplier("Amazon", "Digital content and services");
-        lenovo = new Supplier("Lenovo", "Computers");
-
         tablet = new ProductCategory("Tablet", "Hardware", "A tablet computer, commonly shortened to tablet, is a thin, flat mobile computer with a touchscreen display.");
-        phone = new ProductCategory("Phone", "Hardware", "Moblie phones. Your mother can ask you what you eaten for lunch through these devices.");
-        notebook = new ProductCategory("Notebook", "Hardware", "Like a tablet but with keyboard");
     }
 
     @BeforeEach
-    public void setup(){
+    public void setup() {
 
         List<Integer> ids = new ArrayList<>();
-        for (Product product: productDataStore.getAll()) {
+        for (Product product : productDataStore.getAll()) {
             ids.add(product.getId());
         }
-        for (Integer id: ids){
+        for (Integer id : ids) {
             productDataStore.remove(id);
         }
     }
 
     @Test
-    public void testAdd_AddNewProduct_ProductAddedToMem(){
+    public void testAdd_AddNewProduct_ProductAddedToMem() {
         Product fire = new Product("Amazon Fire", 49.9f, "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", tablet, amazon);
         productDataStore.add(fire);
         assertEquals(fire, productDataStore.getAll().get(0));
     }
-//
-//    @Test
-//    public void testFind_FindSupplier_ReturnSupplierWithId(){
-//        Supplier amazon = new Supplier("Amazon", "Digital content and services");
-//        supplierDataStore.add(amazon);
-//        assertEquals(amazon, supplierDataStore.find(1));
-//    }
-//
-//    @Test
-//    public void testRemove_RemoveSupplier_SupplierRemovedFromMem(){
-//        Supplier amazon = new Supplier("Amazon", "Digital content and services");
-//        supplierDataStore.add(amazon);
-//        supplierDataStore.remove(1);
-//        assertEquals(0, supplierDataStore.getAll().size());
-//    }
-//
-//    @Test
-//    public void testFind_FindSupplierIfNoSupplier_ReturnNull(){
-//        assertEquals(null, supplierDataStore.find(1));
-//    }
-//
-//    @Test
-//    public void testRemove_RemoveSupplierIfThereIsNone_ProgramContinuesRunning(){
-//        supplierDataStore.remove(1);
-//    }
-//
-//    @Test
-//    public void testGetAll_GetAllIfNothingInList_ReturnEmptyList(){
-//        List<Supplier> suppliers = new ArrayList<>();
-//        assertEquals(suppliers, supplierDataStore.getAll());
-//    }
-//
-//    @Test
-//    public void testGetAll_GetAllIfSuppliersInList_ReturnSuppliersList(){
-//        Supplier amazon = new Supplier("Amazon", "Digital content and services");
-//        List<Supplier> suppliers = new ArrayList<>();
-//        suppliers.add(amazon);
-//        supplierDataStore.add(amazon);
-//        assertEquals(suppliers, supplierDataStore.getAll());
-//    }
-//
 
-}
+    @Test
+    public void testFind_FindSupplier_ReturnProductWithId() {
+        Product fire = new Product("Amazon Fire", 49.9f, "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", tablet, amazon);
+        productDataStore.add(fire);
+        assertEquals(fire, productDataStore.find(1));
+ }
+
+    @Test
+    public void testRemove_RemoveProduct_ProductRemovedFromMem(){
+        Product fire = new Product("Amazon Fire", 49.9f, "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", tablet, amazon);
+        productDataStore.add(fire);
+        productDataStore.remove(1);
+        assertEquals(0, productDataStore.getAll().size());
+    }
+
+    @Test
+    public void testFind_FindProductIfNoProduct_ReturnNull(){
+        assertEquals(null, productDataStore.find(1));
+    }
+
+    @Test
+    public void testRemove_RemoveProductIfThereIsNone_ProgramContinuesRunning(){
+        productDataStore.remove(1);
+    }
+
+    @Test
+    public void testGetAll_GetAllIfNothingInList_ReturnEmptyList(){
+        List<Product> products = new ArrayList<>();
+        assertEquals(products, productDataStore.getAll());
+    }
+
+    @Test
+    public void testGetAll_GetAllIfProductsInList_ReturnProductsList(){
+        Product fire = new Product("Amazon Fire", 49.9f, "USD", "Fantastic price. Large content ecosystem. Good parental controls. Helpful technical support.", tablet, amazon);
+        List<Product> products = new ArrayList<>();
+        products.add(fire);
+        productDataStore.add(fire);
+        assertEquals(products, productDataStore.getAll());
+    }
+
+    @Test
+    public void testGetAllBy_Suppliers_GetAllIfNothingInList_ReturnEmptyList(){
+        List<Product> products = new ArrayList<>();
+        assertEquals(products, productDataStore.getBy(amazon));
+    }
+
+    @Test
+    public void testGetAllBy_Suppliers_GetAllIfProductsInList_ReturnProductsList(){
+        List<Product> products = prepareProductDataStoreWithOneProduct();
+        assertEquals(products, productDataStore.getBy(amazon));
+    }
+
+    @Test
+    public void testGetAllBy_ProductCategory_GetAllIfNothingInList_ReturnEmptyList(){
+        List<Product> products = new ArrayList<>();
+        assertEquals(products, productDataStore.getBy(tablet));
+    }
+
+    @Test
+    public void testGetAllBy_ProductCategory_GetAllIfProductsInList_ReturnProductsList(){
+        List<Product> products = prepareProductDataStoreWithOneProduct();
+        assertEquals(products, productDataStore.getBy(tablet));
+    }
+
+
+    }
