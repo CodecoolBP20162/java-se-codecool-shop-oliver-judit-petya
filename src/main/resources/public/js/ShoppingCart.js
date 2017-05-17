f0e6a7bd73b86892246db3ad90a91915349d6a24
szkrecsi <ecsenszki.judit@gmail.com>
2017-05-17 09:58:33 +0200

    productTotalPrice in ajax success is added

--- src/main/resources/public/js/ShoppingCart.js
+++ src/main/resources/public/js/ShoppingCart.js
@@ -2,6 +2,9 @@ $(document).ready(function(){
     $('[id="appendedInputButtons"]').on("keyup mouseup", function(){
         var productId = $(this).attr("name");
         var productQuantity = $(this).val();
+        var productPriceID = $("#"+productId);
+        var productPrice = productPriceID.attr("name");
+        var productTotalPrice = Number(productPrice) * Number(productQuantity);
         var link = "/editcart";
         $.ajax({
             url: link,
@@ -9,6 +12,7 @@ $(document).ready(function(){
             data: {"cart-id": productId, "cart-quantity": productQuantity},
             datatype: "html",
             success: function() {
+                productPriceID.html(String(productTotalPrice)+" USD");
             }
         });
     });
